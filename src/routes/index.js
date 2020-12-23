e5f7932c7d7919a02fe5a5a7aa44f383198d9a34
sooyoung ho <ghtndud1993@gmail.com>
2020-12-23 18:21:35 +0900

    no message

--- src/routes/index.js
+++ src/routes/index.js
@@ -21,7 +21,7 @@ Vue.use(Router)
 export const router = new Router({
     mode: 'history',
     routes: [{
-            path: '/',
+            path: '/main',
             name: 'main',
             component: Main
         },
